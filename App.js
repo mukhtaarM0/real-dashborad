0a40c6324c3ddf3919cff74b8487ed43b0403d20
Mukhtaar Maxamed Abdi Egal <147027052+mukhtaarM0@users.noreply.github.com>
2025-06-16 07:45:47 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Dashboard from "./Component/Dashboard";
+import Navbar from './Component/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Navbar/> */}
+      <Dashboard/>
+    </div>
+  );
+}
+
+export default App;
